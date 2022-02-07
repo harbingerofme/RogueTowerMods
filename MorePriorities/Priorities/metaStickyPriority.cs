8fd349038132f796ae10d193b6e475e2e0628573
HarbingerOfMe <harbingerofme@gmail.com>
2022-02-07 19:03:32 +0100

    add Sticky Priority

---
+++ MorePriorities/Priorities/metaStickyPriority.cs
@@ -0,0 +1,19 @@
+﻿using TargetPriorityOrdering;
+
+namespace MorePriorities.Priorities
+{
+    public class StickyPriority : PriorityHandler
+    {
+        public override string Name => "Sticky";
+
+        public override float GetPriorityForTarget(PrioritiserTarget prioritiserTarget)
+        {
+            if (prioritiserTarget.enemy.gameObject == prioritiserTarget.tower.currentTarget)
+            {
+                return 200;
+            }
+
+            return 1;
+        }
+    }
+}
