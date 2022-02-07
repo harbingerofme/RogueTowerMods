f7464bcc04e13e5ff1411c1590d92ec29cf1bea3
HarbingerOfMe <harbingerofme@gmail.com>
2022-02-07 19:03:09 +0100

    Create MorePriorities plugin

---
+++ MorePriorities/Plugin.cs
@@ -0,0 +1,21 @@
+﻿using BepInEx;
+using System;
+using System.Linq;
+using System.Reflection;
+using TargetPriorityOrdering;
+
+namespace MorePriorities
+{
+    [BepInPlugin("harbingerOfMe.morePriorities", "More priorities", "1.0.0")]
+    [BepInDependency("harbingerOfMe.TargetPriorityOrdering", BepInDependency.DependencyFlags.HardDependency)]
+    public class Plugin : BaseUnityPlugin
+    {
+        private void Awake()
+        {
+            foreach (Type t in Assembly.GetExecutingAssembly().GetTypes().Where(t => t.IsSubclassOf(typeof(TargetPriorityOrdering.PriorityHandler))))
+            {
+                OrderedPriorities.AddCustomPriority((PriorityHandler)Activator.CreateInstance(t));
+            }
+        }
+    }
+}
