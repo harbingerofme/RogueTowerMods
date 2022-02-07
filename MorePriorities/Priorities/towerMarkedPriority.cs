abfb455d097d5c9ac1f46916ccda9fea9cc9af79
HarbingerOfMe <harbingerofme@gmail.com>
2022-02-07 19:05:00 +0100

    add marked prio

---
+++ MorePriorities/Priorities/towerMarkedPriority.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TargetPriorityOrdering;
+
+namespace MorePriorities.Priorities
+{
+    public class towerMarkedPriority : TargetPriorityOrdering.PriorityHandler
+    {
+        public override string Name => "Marked";
+
+        public override float GetPriorityForTarget(PrioritiserTarget prioritiserTarget)
+        {
+            return prioritiserTarget.enemy.mark != null ? 2f : 1f;
+        }
+    }
+}
