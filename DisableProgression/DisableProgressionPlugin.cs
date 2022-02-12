d7044abc9fe59f9fce0aaf13524efb496c15678f
HarbingerOfMe <harbingerofme@gmail.com>
2022-02-12 18:32:58 +0100

    Add DisableProgression

---
+++ DisableProgression/DisableProgressionPlugin.cs
@@ -0,0 +1,70 @@
+﻿using BepInEx;
+using MonoMod.RuntimeDetour;
+using BepInEx.Configuration;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace DisableProgression
+{
+    [BepInPlugin("Harb.DisableProgression", PluginInfo.PLUGIN_NAME, PluginInfo.PLUGIN_VERSION)]
+    public class DisableProgressionPlugin : BaseUnityPlugin
+    {
+        private readonly List<Hook> hooks = new();
+
+        public static DisableProgressionPlugin instance;
+
+        public void Awake()
+        {
+            instance = this;
+
+            var isEnabled = Config.Bind(new ConfigDefinition("", "Enabled"), true, new ConfigDescription("If you see this mod as a dependency, its because the mod author has requested for progression to be disabled. However, it's your game: do what you want.")).Value;
+
+            if (!isEnabled)
+            {
+                return;
+            }
+
+            var voidM = typeof(DisableProgressionPlugin).GetMethod(nameof(voidMethod), System.Reflection.BindingFlags.Static | System.Reflection.BindingFlags.NonPublic);
+            var stringM = typeof(DisableProgressionPlugin).GetMethod(nameof(stringMethod), System.Reflection.BindingFlags.Static | System.Reflection.BindingFlags.NonPublic);
+            var intM = typeof(DisableProgressionPlugin).GetMethod(nameof(intMethod), System.Reflection.BindingFlags.Static | System.Reflection.BindingFlags.NonPublic);
+
+            var methods = typeof(AchievementManager).GetMethods(System.Reflection.BindingFlags.DeclaredOnly).Where((m) => m.ReturnType == typeof(void));
+            foreach (var method in methods)
+            {
+                var paramCount = method.GetParameters().Length;
+                Logger.LogDebug(method.Name);
+                switch (paramCount)
+                {
+                    case 0: hooks.Add(new Hook(method, voidM)); break;
+                    case 1:
+                        var parM = method.GetParameters()[0].ParameterType;
+                        if (parM == typeof(string))
+                        {
+                            hooks.Add(new Hook(method, stringM));
+                        } else if (parM == typeof(int)) {
+                            hooks.Add(new Hook(method, intM));
+                        }
+                        break;
+                }
+
+            }
+        }
+
+        private delegate void intDelegate(AchievementManager am, int s);
+        private delegate void stringDelegate(AchievementManager am, string s);
+        private delegate void voidDelegate(AchievementManager am);
+
+        private static void voidMethod(voidDelegate del, AchievementManager _0) { instance.Logger.LogDebug($"Call to {del.Method.Name} blocked."); }
+        private static void stringMethod(stringDelegate del, AchievementManager _0, string _1) { instance.Logger.LogDebug($"Call to {del.Method.Name} blocked."); }
+        private static void intMethod(intDelegate del, AchievementManager _0, int _1) { instance.Logger.LogDebug($"Call to {del.Method.Name} blocked."); }
+
+        public void OnDestroy()
+        {
+            foreach (Hook hook in hooks)
+            {
+                hook.Undo();
+                hook.Free();
+            }
+        }
+    }
+}
---
+++ DisableProgression/Thunderstore/README.md
@@ -0,0 +1,11 @@
+It has been requested that mods can stop triumphs from being unlocked while active.
+This mod does just that, no triumphs/achievements/challenges/whatevermajigs.
+
+It does, however, allow xp.
+
+---
+
+For developers:
+
+add this to your manifest to have modmanagers automatically enable it.
+Use `[BepinDependency("DisableProgression", BepInDependency.DependencyFlags.HardDependency)]` at the top of your plugin class to prevent your mod loading without this mod installed.
